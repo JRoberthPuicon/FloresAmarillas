93686fe9ca7468fb80453b619763ba684a9b9a1d
JRoberthPuicon <joseluzmanuelito@gmail.com>
2024-09-10 10:26:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+        const textElement = document.querySelector('.text');
+        textElement.classList.add('visible');
+});
